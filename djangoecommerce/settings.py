97d124ccde3a4d380818634030126db74c2d5a6b
slocksert <rsdlj01@gmail.com>
2023-05-31 14:10:29 -0300

    Testing basedir

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = f'{BASE_DIR}/staticfiles/'
 STATIC_ROOT = f"{BASE_DIR}/staticfiles/"
 
 # Default primary key field type
