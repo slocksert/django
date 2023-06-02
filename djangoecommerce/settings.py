f49ab4c565dd93c809a3e9b9b6ecf629f5568e0e
slocksert <rsdlj01@gmail.com>
2023-06-01 22:49:06 -0300

    Postgres DB

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'djangoecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'LsTHZuIcomxwEbnuROMF',
+        'HOST': 'containers-us-west-44.railway.app',
+        'PORT': '7392',
     }
 }
 
