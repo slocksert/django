ed30fda52c2e56e93fdbcec3f35a32c5a4515d0f
slocksert <rsdlj01@gmail.com>
2023-06-05 14:57:18 -0300

    Adding string name method

--- catalog/models.py
+++ catalog/models.py
@@ -14,6 +14,9 @@ class Category(models.Model):
         verbose_name_plural = "Categorias"
         ordering = ['name']
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
 
     name = models.CharField("Nome", max_length=100)
@@ -29,3 +32,6 @@ class Product(models.Model):
         verbose_name = "Produto"
         verbose_name_plural = "Produtos"
         ordering = ["name"]
+    
+    def __str__(self):
+        return self.name
