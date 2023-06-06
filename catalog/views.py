00f620ddb8573a3f2942584de4d20bfd0dbdfd03
slocksert <rsdlj01@gmail.com>
2023-06-05 21:44:49 -0300

    creating catalog views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+from .models import Product, Category
+
 # Create your views here.
+def product_list(request):
+    context = {
+        'product_list': Product.objects.all()
+    }
+    return render(request, 'catalog/product_list.html', context=context)
+
+def category(request, slug):
+    category = Category.objects.get(slug=slug)
+    context = {
+        "current_category": category,
+        "product_list": Product.objects.filter(category=category),
+    }
+    return render(request, 'catalog/category.html', context)
+
+def product(request, slug):
+    product = Product.objects.get(slug=slug)
+    context = {
+        'product':product
+    }
+    return render(request, 'catalog/product.html', context)
