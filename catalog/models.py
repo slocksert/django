e0d53d54a24d0ad167447c936aaf52b9a7c3d939
slocksert <rsdlj01@gmail.com>
2023-06-05 21:43:42 -0300

    absolute url

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -16,6 +17,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('catalog:category', kwargs={'slug': self.slug})
 
 class Product(models.Model):
 
@@ -35,3 +39,6 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('catalog:product', kwargs={'slug': self.slug})
