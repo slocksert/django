74cda60f3284d38283ccb51088ad5d909d083132
slocksert <rsdlj01@gmail.com>
2023-06-05 21:53:14 -0300

    changing products to catalog and including

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('contato/', views.contact, name="contact"),
-    path('produtos/', views.product_list, name="products"),
-    path('produto/', views.product, name="product")
+    path('catalog/', include(("catalog.urls", 'catalog'), namespace="catalog")),
 ]
