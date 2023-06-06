646c3679b30524d77e258ceea08e4704ea1acd18
slocksert <rsdlj01@gmail.com>
2023-06-05 22:10:54 -0300

    debug False

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-41vwgrie!&t4ig+2yd)34t0%wn*8wcr@7emi87vx#o3pc7*p0@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
